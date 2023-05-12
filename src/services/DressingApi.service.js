af63c63b99d9324724d93186a0f1ea8ea5ca527c
fannyarles <fanny.arles@orange.fr>
2023-05-12 08:38:03 +0200

    trigger cyclic deployment

--- src/services/DressingApi.service.js
+++ src/services/DressingApi.service.js
@@ -4,7 +4,7 @@ const storedToken = localStorage.getItem('authToken');
 const api = axios.create({
     // make sure you use PORT = 5005 (the port where our server is running)
     baseURL: process.env.REACT_APP_API_URL
-    // withCredentials: true // => you might need this option if using cookies and sessions
+    // withCredentials: true // => you might need this option if using cookies and sessions.
 });
 
 const addItem = ({ category, brand, occasions, imageUrl, ownerId }) => {
